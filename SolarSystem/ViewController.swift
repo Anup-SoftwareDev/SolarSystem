74dc288e63b5dabcfb94af0fee86c96cce1497e2
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2023-07-07 13:34:41 +1000

    planet details completed

--- SolarSystem/ViewController.swift
+++ SolarSystem/ViewController.swift
@@ -9,15 +9,16 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     let planets = [
-        Planet(name: "Sun", image: "Sun"),
-        Planet(name: "Mercury", image: "Mercury"),
-        Planet(name: "Venus", image: "Venus"),
-        Planet(name: "Earth", image: "Earth"),
-        Planet(name: "Mars", image: "Mars"),
-        Planet(name: "Jupiter", image: "Jupiter"),
-        Planet(name: "Saturn", image: "Saturn"),
-        Planet(name: "Neptune", image: "Neptune"),
-        Planet(name: "Pluto", image: "Pluto")
+        //Planet(name: "Sun", image: "Sun", mass: 0.33, diameter: 4879, gravity: 3.7, distanceSun: 0, temperature: 167, dayLength: 0, moons: 0, orbitalPeriod: 0),
+        Planet(name: "Mercury", image: "Mercury", mass: 0.33, diameter: 4879, gravity: 3.7, distanceSun: 57.9, temperature: 167, dayLength: 4222.6, moons: 0, orbitalPeriod: 88),
+        Planet(name: "Venus", image: "Venus", mass: 4.87, diameter: 12104, gravity: 8.9, distanceSun: 108.2, temperature: 464, dayLength: 2802, moons: 0, orbitalPeriod: 224.7),
+        Planet(name: "Earth", image: "Earth", mass: 5.97, diameter: 12756, gravity: 9.8, distanceSun: 149.6, temperature: 15, dayLength: 24, moons: 1, orbitalPeriod: 365.2),
+        Planet(name: "Mars", image: "Mars", mass: 0.642, diameter: 6792, gravity: 3.7, distanceSun: 228, temperature: -65, dayLength: 24.7, moons: 2, orbitalPeriod: 687),
+        Planet(name: "Jupiter", image: "Jupiter", mass: 1898, diameter: 142984, gravity: 23.1, distanceSun: 778.5, temperature: -110, dayLength: 9.9, moons: 92, orbitalPeriod: 4331),
+        Planet(name: "Saturn", image: "Saturn", mass: 568, diameter: 120536, gravity: 9, distanceSun: 1432, temperature: -140, dayLength: 10.7, moons: 83, orbitalPeriod: 10747),
+        Planet(name: "Uranus", image: "Uranus", mass: 86.8, diameter: 51118, gravity: 9, distanceSun: 2867, temperature: -195, dayLength: 17.2, moons: 27, orbitalPeriod: 30589),
+        Planet(name: "Neptune", image: "Neptune", mass: 102, diameter: 49528, gravity: 8.7, distanceSun: 4515, temperature: -200, dayLength: 16.1, moons: 14, orbitalPeriod: 59800),
+        Planet(name: "Pluto", image: "Pluto", mass: 0.0130, diameter: 2376, gravity: 0.7, distanceSun: 5906.4, temperature: -225, dayLength: 153, moons: 5, orbitalPeriod: 90560)
     ]
     
     let tableView = UITableView()
@@ -67,11 +68,36 @@ func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> U
     cell.planetName.text = planet.name
     return cell
 }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            print("Preparing for segue: \(segue.identifier ?? "No identifier")")
+            print("Segue destination VC: \(segue.destination)")
+            
+            if let indexPath = tableView.indexPathForSelectedRow {
+                print("Selected index path: \(indexPath)")
+            } else {
+                print("No row selected")
+            }
+        if segue.identifier == "GoToPlanetDetail",
+           
+           let detailVC = segue.destination as? PlanetDetailViewController,
+           let indexPath = tableView.indexPathForSelectedRow {
+            // Pass the selected planet to PlanetDetailViewController
+            detailVC.planet = planets[indexPath.row]
+            print(planets[indexPath.row].name)
+            print("it is in prepare")
+        }
+    }
 
-func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    tableView.deselectRow(at: indexPath, animated: true)
-    print("cell of \(planets[indexPath.row].name) is clicked")
-}
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        print("cell of \(planets[indexPath.row].name) is clicked")
+
+        // Perform the segue
+        performSegue(withIdentifier: "GoToPlanetDetail", sender: self)
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
 
 }
 
@@ -104,7 +130,6 @@ class CustomCell: UITableViewCell {
 
         // Change the font size and color of the planetName label
         planetName.font = UIFont(name: "ArialRoundedMTBold", size: 40)
-        //planetName.font = UIFont.systemFont(ofSize: 35) // Change the number for a bigger or smaller font
         planetName.textColor = UIColor.white // Change the color as needed
         
         // Center the text vertically and horizontally
@@ -127,4 +152,12 @@ class CustomCell: UITableViewCell {
 struct Planet {
     var name: String
     var image: String
+    var mass: Double
+    var diameter: Int
+    var gravity: Double
+    var distanceSun: Double
+    var temperature: Int
+    var dayLength: Double
+    var moons: Int
+    var orbitalPeriod: Double
 }
